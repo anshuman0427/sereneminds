a264153e251c79b4bce9de8e73ed700eab0df8a2
Anshuman Mishra <anshuman0427@gmail.com>
2025-01-18 17:25:53 +0530

    Added Vercel Analytics and Speed Insights integration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LaunchingSoonPage from './components/LaunchingSoonPage';
 import PrivacyPolicy from './components/PrivacyPolicy'; // New import
+import ShippingPolicy from './components/ShippingPolicy';
+import RefundandCancellation from './components/RefundandCancellation';
+import TermsandConditions from './components/TermsandConditions';
+import ContactUS from './components/ContactUs';
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LaunchingSoonPage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/Contact-Us" element={<ContactUS />} />
+        <Route path="/Shipping-policy" element={<ShippingPolicy />} />
+        <Route path="/Refund-and-cancellation" element={<RefundandCancellation />} />
+        <Route path="/Terms-and-conditions" element={<TermsandConditions />} />
       </Routes>
     </Router>
   );
